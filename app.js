5989144201081f42d3cf98053ec08d23281002df
Alex Evans <alexdavidevans@gmail.com>
2022-01-25 16:20:57 +1300

    Fixed menu buttons

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const items = [
         title: "Candide Thovex 3.0 Skis",
         category: "skis",
         price: 699.99,
-        img: "./images/candide-3.0.png",
+        img: "./images/candide3.0.jpeg",
         desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolores enim modi quis consectetur debitis ipsum eos, id mollitia pariatur.`
     },
     {
@@ -49,13 +49,16 @@ const items = [
     },
 ];
 
+// Variables 
+const sectionCenter = document.querySelector('.section-center');
 
-const sectionCenter = document.querySelector('section-center');
-
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
+
+// Event listerners -- Load items
 
 navToggle.addEventListener('click', function () {
     links.classList.toggle('show-links');
@@ -64,6 +67,29 @@ navToggle.addEventListener('click', function () {
 window.addEventListener('DOMContentLoaded', function(){
     displayMenuItems(items);
 });
+
+// filter items
+
+filterBtns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const category = e.currentTarget.dataset.id;
+        const itemCategory = items.filter(function(menuItem) {
+            if(menuItem.category === category) {
+                return menuItem;
+            }
+        });
+        // console.log(itemCategory);
+        if(category === 'all') {
+            displayMenuItems(items)
+        } else {
+            displayMenuItems(itemCategory)
+        }
+    });
+});
+
+
+
+// functions
 
 function displayMenuItems (menuItems) {
     let displayMenu = menuItems.map(function(item){
@@ -74,7 +100,7 @@ function displayMenuItems (menuItems) {
                     <div class="item-info">
                         <header>    
                             <h4>${item.title}</h4>
-                            <h4 class=${item.price}>$15</h4>
+                            <h4 class="price">$${item.price}</h4>
                         </header>
                         <p class="item-text">${item.desc}</p>
                     </div>
